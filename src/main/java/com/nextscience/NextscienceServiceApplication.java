e9d4485e2dceafcd4c2547a267f00b16981921e4
rbasuvaraj <rbasuvaraj@tikamobile.com>
2023-10-20 08:25:08 +0530

    Server

--- src/main/java/com/nextscience/NextscienceServiceApplication.java
+++ src/main/java/com/nextscience/NextscienceServiceApplication.java
@@ -2,6 +2,8 @@ package com.nextscience;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.nextscience.Constants.UsersConstant;
 
@@ -9,16 +11,21 @@ import io.swagger.v3.oas.models.annotations.OpenAPI30;
 
 /**
  * Processes an {@link NextscienceServiceApplication } request.
+ * 
  * @author Raghu
  *
  */
 
 @SpringBootApplication
 
-public class NextscienceServiceApplication {
+public class NextscienceServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextscienceServiceApplication.class, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(NextscienceServiceApplication.class);
+	}
 }
