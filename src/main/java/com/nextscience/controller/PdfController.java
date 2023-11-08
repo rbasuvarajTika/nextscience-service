ae82084f10c072818e34f37209d04d6d6fc8c24d
unknown <rbasuvaraj@tikamobile.com>
2023-11-08 09:44:00 +0530

    PDF Split Chnages

--- src/main/java/com/nextscience/controller/PdfController.java
+++ src/main/java/com/nextscience/controller/PdfController.java
@@ -1,6 +1,12 @@
 package com.nextscience.controller;
 
 import java.io.File;
+//Splitting a PDF in to many using Java 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.multipdf.Splitter;
@@ -11,27 +17,24 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nextscience.Constants.CommonConstants;
+import com.nextscience.dto.response.NSServiceResponse;
 import com.nextscience.entity.FaxRx;
-
-
-
-//Splitting a PDF in to many using Java 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.Iterator;
+import com.nextscience.enums.ErrorCodes;
+import com.nextscience.exceptions.NSException;
+import com.nextscience.service.FaxRxService;
+import com.nextscience.utility.ResponseHelper;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(CommonConstants.APIV1FAX)
 public class PdfController {
 	
+	
+	@Autowired
+	private FaxRxService faxRxService;
 
 	@GetMapping("/splitPdf")
 	@CrossOrigin(origins = "*")
@@ -85,6 +88,60 @@ public class PdfController {
 	}
 	
 	
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("/splitPdfByRange/{faxId}/{fromPage}/{toPage}")
+	public NSServiceResponse<String> splitPdf(@PathVariable String faxId, @PathVariable int fromPage,
+			@PathVariable int toPage) throws MalformedURLException, IOException {
+		//File pdffile = new File("C:/PDF-TASK/sample.pdf");
+		FaxRx faxRxResponse = faxRxService.fetchListById(faxId);
+		InputStream is;
+		String ftpUrl = faxRxResponse.getFaxUrl();
+		is = new URL(ftpUrl).openStream();
+		Splitter splitting = new Splitter();
+		//Splitter splittingRemain = new Splitter();
+		//List<PDDocument> Page;
+		try {
+			PDDocument document = Loader.loadPDF(is.readAllBytes());
+			int totalPages = document.getNumberOfPages();
+			String errorMessage ="Range is Invalid";
+			if(totalPages < fromPage) {
+				throw new NSException(ErrorCodes.OK, errorMessage);
+			}else if (totalPages < toPage) {
+				throw new NSException(ErrorCodes.OK, errorMessage);
+			}else if (fromPage > toPage) {
+				throw new NSException(ErrorCodes.OK, errorMessage);
+			}
+
+			splitting.setStartPage(fromPage);
+			splitting.setEndPage(toPage);
+			splitting.setSplitAtPage(toPage - fromPage + 1);
+			
+			
+			List<PDDocument> lst = splitting.split(document);
+			PDDocument pdfDocPartial = lst.get(0);
+			File fRange = new File("C:/PDF-TASK/splitfax"+faxId+".pdf");
+			pdfDocPartial.save(fRange);
+			/*
+			 * int splitNewPageFrom = toPage + 1; int splitNewPageTo = totalPages;
+			 * splittingRemain.setStartPage(splitNewPageFrom);
+			 * splittingRemain.setEndPage(splitNewPageTo);
+			 * splittingRemain.setSplitAtPage(splitNewPageTo - splitNewPageFrom + 1);
+			 * 
+			 * List<PDDocument> lst1 = splittingRemain.split(document); PDDocument
+			 * pdfDocPartial1 = lst1.get(0); File fRange1 = new
+			 * File("C:/PDF-TASK/sample-range-remain.pdf"); pdfDocPartial1.save(fRange1);
+			 */
+			document.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return ResponseHelper.createResponse(new NSServiceResponse<String>(), "Pdf Splitted Sucessfully",
+				CommonConstants.SUCCESSFULLY, CommonConstants.ERRROR);
+
+	}
 
 
 
