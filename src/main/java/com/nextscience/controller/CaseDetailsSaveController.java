d8e140e9d715394d7c82abd0ee724b10fd93bdb9
unknown <rbasuvaraj@tikamobile.com>
2023-12-15 20:45:56 +0530

    search for patient name,hcp name

--- src/main/java/com/nextscience/controller/CaseDetailsSaveController.java
+++ src/main/java/com/nextscience/controller/CaseDetailsSaveController.java
@@ -3,7 +3,9 @@ package com.nextscience.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -252,9 +254,9 @@ public class CaseDetailsSaveController {
 	}
 
 	@SuppressWarnings("unchecked")
-	@PutMapping("/updateFaxRxConfirm")
+	@PutMapping(value="/updateFaxRxConfirm")
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
-	public NSServiceResponse<String> updateFaxRxConfirm(@RequestBody FaxRxConfirmRequest request) {
+	public NSServiceResponse<CaseDetailsSaveRequest> updateFaxRxConfirm(@RequestBody FaxRxConfirmRequest request) {
 
 		String response = caseDetailsSaveService.updateFaxRxConfirm(request);
 
